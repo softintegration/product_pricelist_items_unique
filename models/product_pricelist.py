493b004c64c0fb49c5bf721952aaecc83933a74a
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-02-07 23:42:04 +0100

    [FIX] Pricelist:Add the product to the constraint of unicity

--- models/product_pricelist.py
+++ models/product_pricelist.py
@@ -8,6 +8,6 @@ class PricelistItem(models.Model):
     _inherit = "product.pricelist.item"
 
     _sql_constraints = [
-        ('pricelist_item_uniq', 'unique(min_quantity,fixed_price,currency_id,date_start,date_end)',
+        ('pricelist_item_uniq', 'unique(product_tmpl_id,min_quantity,fixed_price,currency_id,date_start,date_end)',
          'Price List item (Product,Min. Quantity,Price,Start Date,End Date) must be unique!'),
     ]
